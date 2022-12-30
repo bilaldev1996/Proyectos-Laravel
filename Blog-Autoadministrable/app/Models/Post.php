97b24f535c56e458554cce7e58ccb25b27fb6be4
bilaldev1996 <bilalmessaoui96@gmail.com>
2022-12-30 02:00:28 +0100

    relations - migrations  - seeders - factories

---
+++ Blog-Autoadministrable/app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // Relacion uno a muchos inversa
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+
+    // Relacion uno a muchos inversa
+    public function category(){
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    // Relacion muchos a muchos
+    public function tags(){
+        return $this->belongsToMany('App\Models\Tag');
+    }
+
+    // Relacion uno a uno polimorfica
+    public function image(){
+        return $this->morphOne('App\Models\Image', 'imageable');
+    }
+}
---
+++ Blog-Autoadministrable/database/factories/ImageFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
+ */
+class ImageFactory extends Factory
+{
+    protected $model = \App\Models\Image::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'url' => 'posts/' . $this->faker->image('public/storage/posts', 650 , 480 , null , false)
+        ];
+    }
+}
